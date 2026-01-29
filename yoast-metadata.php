3664ab8d17ff539efe3e524905efd1acf500b7b1
Marius Paduraru <marius.paduraru@gmail.com>
2026-01-29 16:49:52 +0000

    Add initial plugin files and update .gitignore

---
+++ yoast-metadata.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Yoast Metadata
+ * Plugin URI:        https://github.com/WagadaDigital/yoast-metadata
+ * Description:       A plugin to manage Yoast SEO metadata.
+ * Requires at least: 6.1
+ * Requires PHP:      7.4
+ * Version:           0.1.1
+ * Author:            Wagada Digital
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       yoast-metadata
+ *
+ * @package WagadaDigital\YoastMetadata
+ */
